6390270a6c3095f36e177dfb88b254d9625c87a6
david ruiz <david.ruiz.co@cloudappi.net>
2023-02-10 07:04:26 -0500

    documentation and central exeption handle

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
 @app.get("/")
 async def read_main():
     return {"msg": "Hello World"}
+
+@app.get("/docs")
+async def read_openApi():
+    return get_openapi(title="FastApi-Demo", version="1.0.0", routes=app.routes)
+    
+@app.exception_handler(Exception)
+async def exception_handler(request, exc):
+    return JSONResponse(status_code=500, content={"message": f"Something went wrong :("})
